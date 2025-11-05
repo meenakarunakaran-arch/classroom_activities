80fd88716d435e933e75514e82fbd1d53f0432ae
meenakarunakaran-arch <107860552+hubdew@users.noreply.github.com>
2025-11-05 18:14:51 +0000

    initial commit

---
+++ activity_3_data_types.py
@@ -0,0 +1,10 @@
+a = int(input("Enter an integer:"))
+b =input("Enter a name:")
+c = float(input("Enter a decimal:"))
+d =bool(input("enter true or false:"))
+#print("Hello world")
+print(type(a),"\n", type(b), "\n", type(c), "\n", type(d))
+x = "Hello"
+y = x[::-1]
+#slicing syntax = [start:end:step]
+print (x,"\n" y)
