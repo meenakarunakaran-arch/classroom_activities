ede0a48d47e07b137cc823b7270ce9da5427134a
meenakarunakaran-arch <107860552+hubdew@users.noreply.github.com>
2026-05-29 19:38:35 +0100

    restaurant

--- no3.py
+++ no3.py
@@ -35,7 +35,7 @@ root.geometry('250x300')
 nums = [[1,2,3 ], [4,5,6], [7,8,9], ['*', 0, '#']]
 for i in range (4):
     root.columnconfigure(i, weight=1, minsize=75)
-    root.rowconfigureconfigure(i, weight=1, minsize=50)
+    root.rowconfigure(i, weight=1, minsize=50)
     for j in range (3):
             frame = Frame(master = root, relief = SUNKEN, borderwidth=1)
             frame.grid(row=i, column=j)
